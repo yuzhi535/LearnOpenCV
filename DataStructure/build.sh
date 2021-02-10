02af8dda66e1146cafa67afdc8637c8c7f0f4d2b
yuzhi535 <zhouyuxi.abc@gmail.com>
2021-02-10 22:34:10 +0800

    update build.sh

--- DataStructure/build.sh
+++ DataStructure/build.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-if test $# -eq 1; then
-    echo -o "Please enter the src file path and the output file name"
-    read -p "src: " src
-    read -p "output: " output
+if test $# -eq 0; then
+    echo "Please enter the src file path and the output file name "
+    read -p "src file path: "  src
+    read -p "output file name: "  output
     g++ ${src} -o ${output} $(pkg-config --libs --cflags opencv) && ./${output}
 else
     g++ $1 -o $2 $(pkg-config --libs --cflags opencv) && ./$2
