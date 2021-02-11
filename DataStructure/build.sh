7cc9b184c26786b076f621949e2d20b6b55bc20f
yuzhi535 <zhouyuxi.abc@gmail.com>
2021-02-11 15:42:57 +0800

    update

--- DataStructure/build.sh
+++ DataStructure/build.sh
@@ -1,15 +1,18 @@
 #! /bin/bash
-# only build, not run!
+# Given that the number ofarguments of an arbitrary program I've wrote, this script only builds the cpp file, not run it!
+# so you must run it by yourself!
+
 if test $# -eq 0; then
     echo "Please enter the src file path and the output file name "
     read -p "src file path: " src
     read -p "output file name: " output
-    g++ ${src} -o ${output}.run $(pkg-config --libs --cflags opencv) && mv ${output}.exe ../bin/
+    g++ ${src} -o ${output} $(pkg-config --libs --cflags opencv) && mv ${output} ../bin/
 
 else
     if test $# -eq 1; then
         g++ $1 $(pkg-config --libs --cflags opencv) && mv a.out ../bin/
     else
-        g++ $1 -o $2.run $(pkg-config --libs --cflags opencv) && mv $2.exe ../bin/
+        g++ $1 -o $2 $(pkg-config --libs --cflags opencv) && mv $2 ../bin/
+        # or g++ $1 -o $2 `pkg-config --libs --cflags opencv` && mv $2 ../bin/
     fi
 fi
